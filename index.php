b6e8a59c76fb482299281f177ff644831798dc5a
oHmz <lnw_oom@hotmail.com>
2018-10-03 02:00:26 +0700

    add twig template

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-ฟหกฟหกฟหกฟหก
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/site/views', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+
+    return $view;
+};
+
+$app->get('/', function($request, $response, $args) {
+    return $this->view->render($response, 'home.twig');
+});
+
+$app->run();
