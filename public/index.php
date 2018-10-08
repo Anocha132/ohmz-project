46d97c4f76c7b056b7562504d0fe78698e33d670
Anocha Karnchanathaworn <lnw_oom@hotmail.com>
2018-10-08 19:48:06 +0700

    Setup something

--- public/index.php
+++ public/index.php
@@ -4,6 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../bootstrap/helper.php';
 require __DIR__ . '/../bootstrap/app.php';
 require __DIR__ . '/../bootstrap/database.php';
-require __DIR__ . '/../routes/site.php';
+require __DIR__ . '/../routers/site.php';
 
 $app->run();
