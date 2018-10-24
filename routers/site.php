93d8a72e30cf349d18f84496b96b93b8c9b694d1
oHmz <lnw_oom@hotmail.com>
2018-10-25 02:02:02 +0700

    add log-in and register

--- routers/site.php
+++ routers/site.php
@@ -3,3 +3,14 @@
 use App\Controllers;
 
 $app->get('/', Controllers\HomeController::class . ':index');
+$app->get('/chapter-1', Controllers\Chapter1Controller::class . ':index');
+$app->get('/chapter-2', Controllers\Chapter2Controller::class . ':index');
+$app->get('/exercise-2', Controllers\Exercise2Controller::class . ':index');
+$app->get('/exercise-1', Controllers\Exercise1Controller::class . ':index');
+$app->get('/exercise-3', Controllers\Exercise3Controller::class . ':index');
+$app->get('/about-us', Controllers\AboutUsController::class . ':index');
+$app->get('/contact-us', Controllers\ContactUsController::class . ':index');
+
+$app->get('/log-in', Controllers\LogInController::class . ':index')->setName('log-in');
+
+$app->get('/register', Controllers\RegisterController::class . ':index')->setName('register');
